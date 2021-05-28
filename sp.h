341c5ad4baeeff0e97b29d110ed15ad101516b83
asherlie <lieber.asher@gmail.com>
2021-05-28 00:26:31 -0400

    defined struct thread_queue and struct spool_t

--- sp.h
+++ sp.h
@@ -1,7 +1,17 @@
+#include <pthread.h>
+
 struct thread{
+    pthread_t pth;
 };
 
 struct thread_queue{
-    
+    /* this is an array for now - no reason for added complexity */
+    struct thread* threads;
+    /* in case we'd like to support dynamic resizing */
+    int n_threads; /* , thread_cap */;
 };
 
+struct spool_t{
+    struct thread_queue* tq;
+    pthread_cond_t spool_up;
+};
