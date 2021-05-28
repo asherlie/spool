e0ceaf44d71a33b00552109482d4606c51e3c9c7
asherlie <lieber.asher@gmail.com>
2021-05-28 00:59:11 -0400

    implemented await_instructions()

--- sp.c
+++ sp.c
@@ -2,17 +2,68 @@
 #include <stdlib.h>
 #include "sp.h"
 
-void init_tq(struct thread_queue* tq, int n_threads){
+struct routine* pop_rq(struct routine_queue* rq){
+    struct routine* ret = NULL;
+
+    pthread_mutex_lock(&rq->rlock);
+    ret = rq->r;
+    if(ret)rq->r = rq->r->next;
+    pthread_mutex_unlock(&rq->rlock);
+
+    return ret;
+}
+
+void* await_instructions(void* v_rq){
+    struct routine_queue* rq = v_rq;
+    struct routine* r;
+
+    pthread_mutex_t tmplck;
+    pthread_mutex_init(&tmplck, NULL);
+    while(1){
+        pthread_mutex_lock(&tmplck);
+        /* it's not guaranteed that only one
+         * thread will be woken up
+         *
+         * to safeguard against this we will attempt
+         * to acquire a lock once we're woken up
+         * to confirm that only one thread will run
+         *
+         * HMM - this may not be an issue
+         * if too many threads are woken up, IE. 
+         * n_threads_awake > n_available_routines
+         * some of the threads are guaranteed
+         * to re-block on cond_t
+         * because pop_rq() is threadsafe
+         */
+        pthread_cond_wait(&rq->spool_up, &tmplck);
+
+        if(!(r = pop_rq(rq)))continue;
+
+        r->func(r->arg);
+
+        pthread_cond_signal(&rq->spool_up);
+        /*pthread_cond_broadcast*/
+    }
+}
+
+void init_tq(struct thread_queue* tq, int n_threads, struct routine_queue* rq){
     tq->threads = malloc(sizeof(struct thread)*n_threads);
     tq->n_threads = n_threads;
     for(int i = 0; i < n_threads; ++i){
         tq->threads[i].thread_id = i;
-        pthread_mutex_init(&tq->threads[i].pth, NULL);
+        pthread_create(&tq->threads[i].pth, NULL, await_instructions, rq);
     }
 }
 
+void init_rq(struct routine_queue* rq){
+    pthread_cond_init(&rq->spool_up, NULL);
+    pthread_mutex_init(&rq->rlock, NULL);
+}
+
 void init_spool_t(struct spool_t* s, int n_threads){
-    pthread_cond_init(&s->spool_up, NULL);
+    /*pthread_cond_init(&s->spool_up, NULL);*/
+    init_rq(s->rq);
+    init_tq(s->tq, n_threads, s->rq);
 }
 
 int main(){
--- sp.h
+++ sp.h
@@ -5,6 +5,7 @@ struct thread{
     pthread_t pth;
 };
 
+/* thread_queue is used only to keep track of all threads */
 struct thread_queue{
     /* this is an array for now - no reason for added complexity */
     struct thread* threads;
@@ -12,7 +13,22 @@ struct thread_queue{
     int n_threads; /* , thread_cap */;
 };
 
+//struct routine;
+
+struct routine{
+    //volatile void* (*func)(void*);
+    void* arg;
+    void* (*func)(void*);
+    struct routine* next;
+};
+
+struct routine_queue{
+    pthread_cond_t spool_up;
+    pthread_mutex_t rlock;
+    struct routine* r;
+};
+
 struct spool_t{
     struct thread_queue* tq;
-    pthread_cond_t spool_up;
+    struct routine_queue* rq;
 };
