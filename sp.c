b90945774904275f10f5ad8d36004fa58f24d8ec
asherlie <lieber.asher@gmail.com>
2021-05-28 00:26:15 -0400

    added init_tq(), init_spool_t()

--- sp.c
+++ sp.c
@@ -1,3 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "sp.h"
 
+void init_tq(struct thread_queue* tq, int n_threads){
+    tq->threads = malloc(sizeof(struct thread)*n_threads);
+    tq->n_threads = n_threads;
+}
+
+void init_spool_t(struct spool_t* s, int n_threads){
+    pthread_cond_init(&s->spool_up, NULL);
+}
+
+int main(){
+}
