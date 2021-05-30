33f6b6d3b9d16fa1abcdb772b69271bb42243c2d
asherlie <lieber.asher@gmail.com>
2021-05-30 02:37:45 -0400

    updated test.c

--- test.c
+++ test.c
@@ -13,14 +13,44 @@ void* tst(void* arg){
 
 int main(){
     struct spool_t s;
-    int* arg;
+    int* arg, count, buf[1000];
 
-    init_spool_t(&s, 20);
-    for(int i = 0; i < 90; ++i){
-        arg = malloc(sizeof(int));
-        *arg = i;
+    init_spool_t(&s, 30);
+    set_routine_target(&s, 178);
+    for(int i = 0; i < 400; ++i){
+        buf[i] = i;
+        arg = buf+i;
         exec_routine(&s, tst, arg);
     }
+    /*destroy_spool_t(&s);*/
+    await_routine_target(&s);
+    return 0;
 
-    usleep(11000000);
+    count = 0;
+    while(getchar()){
+        if(count++ %2 == 0)
+            pause_exec(&s);
+        else
+            resume_exec(&s);
+    }
+
+    pause_exec(&s);
+    /*usleep(10000);*/
+    /*puts("PAUSING FOR");*/
+    for(int i = 0; i < 5; ++i){
+        printf("%i", i);
+        fflush(stdout);
+        usleep(250000);
+        putchar('.');
+        fflush(stdout);
+        usleep(250000);
+        putchar('.');
+        fflush(stdout);
+    }
+    puts("5");
+    resume_exec(&s);
+    pause_exec(&s);
+    resume_exec(&s);
+    pause_exec(&s);
+    usleep(10000000);
 }
