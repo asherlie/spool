6aff7e63a0a69f349aea9e85d84b9867f928755e
asherlie <lieber.asher@gmail.com>
2021-06-29 23:25:39 -0400

    updated exec_routine() header and struct routine to work with new await_single behavior

--- sp.h
+++ sp.h
@@ -23,6 +23,9 @@ struct routine{
     void* arg;
     void* (*func)(void*);
     struct routine* next;
+
+    /* used to wait on a single routine */
+    pthread_cond_t* running;
 };
 
 struct routine_queue{
@@ -53,9 +56,13 @@ struct spool_t{
     struct routine_queue rq;
 };
 
+/* is used only as the return value of exec_routine()
+ * and to wait for a routine to finish running
+ */
+
 void init_spool_t(struct spool_t* s, int n_threads);
-void exec_routine(struct spool_t* s, void* (*func)(void*),
-                                     void* arg);
+struct routine* exec_routine(struct spool_t* s, void* (*func)(void*),
+                                     void* arg, _Bool create_cond);
 void pause_exec(struct spool_t* s);
 void resume_exec(struct spool_t* s);
 /* this is inexact by nature
@@ -69,3 +76,18 @@ void resume_exec(struct spool_t* s);
 void set_routine_target(struct spool_t* s, int target);
 _Bool await_routine_target(struct spool_t* s);
 void destroy_spool_t(struct spool_t* s);
+
+#if !1
+i want to have the option of waiting for a single thread to finish running
+something like:
+    await_routine_single(spool_t, thread_id)
+
+    thread_id is a timestamp added to the pointer of func
+    it is created in and returned by exec_routine()
+
+    each thread pthread_cond_signals a unique 
+
+    optional argument for exec_routine(), a pointer to a
+    pthread_cond_t that will be signaled once the routine is done
+    in addition to the spool_up cond
+#endif
